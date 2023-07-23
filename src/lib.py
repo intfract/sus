1bcf67ecd9948e9231d72dab86c7809dae8a6e3e
Int Fract <110207136+intfract@users.noreply.github.com>
2023-07-23 11:13:50 +0000

    added boolean logic

--- src/lib.py
+++ src/lib.py
@@ -1,4 +1,10 @@
 from tokens import Token, Integer, Float, Text, Reference, Keyword, Block, Open, Close, Separator
+
+def restrict_bool(*args):
+    for arg in args:
+        if (arg not in (0, 1)):
+            raise TypeError("boolean arguments must be bit type")
+
 def add(x, y):
     if isinstance(x, Integer) and isinstance(y, Integer):
         return Integer(x + y)
@@ -6,4 +12,30 @@ def add(x, y):
         return Float(x + y)
 
 def multiply(x, y):
-    return Integer(x * y)
+    if isinstance(x, Integer) and isinstance(y, Integer):
+        return Integer(x * y)
+    if isinstance(x, Float) or isinstance(y, Float):
+        return Float(x * y)
+
+def bool_and(x, y):
+    x = int(x.value)
+    y = int(y.value)
+    restrict_bool(x, y)
+    return Integer(int(x and y))
+
+def bool_or(x, y):
+    x = int(x.value)
+    y = int(y.value)
+    restrict_bool(x, y)
+    return Integer(int(x or y))
+
+def bool_xor(x, y):
+    x = int(x.value)
+    y = int(y.value)
+    restrict_bool(x, y)
+    return Integer(int((x and not y) or (not x and y)))
+
+def bool_not(x):
+    x = int(x.value)
+    restrict_bool(x)
+    return Integer(int(not x))
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from lib import add, multiply
+from lib import add, multiply, bool_and, bool_or, bool_xor, bool_not
 from lexer import Lexer
 from interpreter import Parser, Interpreter
 
@@ -6,6 +6,10 @@ def main():
     memory = {
         "add": add,
         "multiply": multiply,
+        "and": bool_and,
+        "or": bool_or,
+        "xor": bool_xor,
+        "not": bool_not,
     }
     code = input()
     tokens = Lexer(code).build()
